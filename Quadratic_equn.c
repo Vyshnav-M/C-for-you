2d12b47d473a58a85cf5b4705a546ff8d3ec7909
VYSHNAV M <61741356+Vyshnav-M@users.noreply.github.com>
2020-03-12 17:28:57 +0530

    Update Quadratic_equn.c

--- Quadratic_equn.c
+++ Quadratic_equn.c
@@ -3,15 +3,20 @@
 #include<math.h>
 void main()
 { 
-	float a,b,c,r1,r2;
+	float a,b,c,det,r1,r2,real,img;
 	printf("Enter the values of a,b,c of the quadratic equation ax^2+bx+c\n");
 	scanf("%f%f%f",&a,&b,&c);
-	if((b*b-4*a*c)<0)
+        det=b*b-4*a*c;
+	if(det<0)
 	{
-	  printf("The roots are imaginery");
-	  return;
+          Real=-b/(2*a);
+          img=sqrt(-det)/(2*a);
+	  printf("The roots are %.2f+%.2f and %.2f-%.2f",real,img,real,img);
 	}
-	r1=(-b+(sqrt(b*b-4*a*c)))/(2*a);
-	r2=(-b-(sqrt(b*b-4*a*c)))/(2*a);
-	printf("The roots are %f  ,  %f",r1,r2);
+        else
+        {
+	r1=(-b+sqrt(det))/(2*a);
+	r2=(-b-sqrt(det))/(2*a);
+	printf("The roots are %.2f  ,  %.2f",r1,r2);
+        }
 }
